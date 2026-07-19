c69278eb34b9fba1d24e84d96fd0f8789e9f7b28
garciaarcosanthony2001-blip <garciaarcosanthony2001@gmail.com>
2026-07-19 15:46:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,22 @@ for (let i = 0; i < 25; i++) {
 
     document.body.appendChild(brillo);
 }
+const fechaBautizo = new Date("August 22, 2026 17:00:00").getTime();
+
+setInterval(function() {
+
+    const ahora = new Date().getTime();
+
+    const diferencia = fechaBautizo - ahora;
+
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+
+    document.getElementById("dias").innerHTML = dias;
+    document.getElementById("horas").innerHTML = horas;
+    document.getElementById("minutos").innerHTML = minutos;
+    document.getElementById("segundos").innerHTML = segundos;
+
+}, 1000);
