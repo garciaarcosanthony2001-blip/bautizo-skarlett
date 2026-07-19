9294fcc7b876f60acb8240d0a996486184d27683
garciaarcosanthony2001-blip <garciaarcosanthony2001@gmail.com>
2026-07-19 15:23:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ function abrirInvitacion() {
         musica.play();
     }
 }
+for (let i = 0; i < 25; i++) {
+    let brillo = document.createElement("div");
+    brillo.className = "destello";
+
+    brillo.style.left = Math.random() * 100 + "vw";
+    brillo.style.animationDelay = Math.random() * 5 + "s";
+    brillo.style.animationDuration = (4 + Math.random() * 5) + "s";
+
+    document.body.appendChild(brillo);
+}
